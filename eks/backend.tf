0e2b8d55883f83e88680d51be830c500907dd6fe
akhildesini <akhildesini@gmail.com>
2025-10-28 13:09:16 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "my-project-bucket-akhil"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
